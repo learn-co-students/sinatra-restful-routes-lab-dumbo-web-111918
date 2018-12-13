03cde4d0eceaf73c01ab05c14746c32501c27bf1
John Mark Redding <johomurk@gmail.com>
2018-12-12 20:14:50 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,7 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :method_override, true
   end
-
-  # code actions here!
 
 end
